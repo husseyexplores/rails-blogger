7bc2cc9647c6c6119dcf6d7d06bf2b162f6a1977
husseyexplores <husseyexplores@gmail.com>
2018-09-04 18:03:10 +0500

    Add article create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,36 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #  Bad idea - controller knows too much
+    # @article = Article.new
+    # @article.title = params[:article][:title]
+    # @article.body = params[:article][:body]
+    # @article.save
+
+    # Better idea
+    # @article = Article.new(
+    #   title: params[:article][:title],
+    #   body: params[:article][:body],
+    #   )
+    # @article.save
+
+    # Even better - no need to make a new hash from an existing hash
+    @article = Article.new(article_params) # article_params is coming from ArticlesHelper
+    @article.save
+
+    redirect_to article_path(@article)
   end
 end
