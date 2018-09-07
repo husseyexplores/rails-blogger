ba7ef36ec29eef06adf211be596638dbea6594fc
husseyexplores <husseyexplores@gmail.com>
2018-09-07 14:15:56 +0500

    Fix JS errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require toastr
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require toastr
 //= require_tree .
 
 toastr.options = {
