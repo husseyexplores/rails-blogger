61a80638eabb4bdbeeb977bb556bd5ef065b153f
husseyexplores <husseyexplores@gmail.com>
2018-09-07 14:08:27 +0500

    Heroku destroy fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require toastr
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
 //= require_tree .
 
 toastr.options = {
